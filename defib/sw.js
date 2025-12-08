506a55fa53b8c008abf7e21e04889912337db170
shakeymedic <shakeymedic@users.noreply.github.com>
2025-12-08 21:34:02 +0000

    Add files via upload

---
+++ defib/sw.js
@@ -0,0 +1,18 @@
+// sw.js - Uninstall script
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      return self.registration.unregister();
+    })
+  );
+});
