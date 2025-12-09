f7e6ce95b5a6cfdb90ab1e089e35cfcd2e518427
shakeymedic <shakeymedic@users.noreply.github.com>
2025-12-09 19:55:04 +0000

    Update sw.js

--- defib/sw.js
+++ defib/sw.js
@@ -1,18 +1,58 @@
-// sw.js - Uninstall script
-self.addEventListener('install', function(e) {
+// defib/sw.js
+const CACHE_NAME = 'wmebem-sim-v17';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './images/logo.png',
+  '../index.html',
+  '../data/engine.js',
+  '../data/scenarios.js',
+  '../data/components.js',
+  '../data/interventions.js',
+  '../data/generators.js',
+  '../data/screens.js',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@17/umd/react.development.js',
+  'https://unpkg.com/react-dom@17/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/lucide@latest',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // We try to cache, but don't fail if external CDNs block CORS opaque responses
+      return cache.addAll(ASSETS_TO_CACHE).catch(err => console.log('Cache warn:', err));
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then((keys) => {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          return caches.delete(cacheName);
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       );
-    }).then(function() {
-      return self.registration.unregister();
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  // Stale-while-revalidate strategy
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      const fetchPromise = fetch(e.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
     })
   );
 });
