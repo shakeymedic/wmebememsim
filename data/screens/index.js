9bc36f46608185dba55c5cb6f0c82658195cbe78
shakeymedic <shakeymedic@users.noreply.github.com>
2025-12-16 16:21:25 +0000

    Create index.js

---
+++ data/screens/index.js
@@ -0,0 +1,10 @@
+// data/screens/index.js
+(() => {
+    // This file now serves as the entry point to load individual screen components.
+    // Ensure index.html loads: setup.js, livesim.js, monitor.js, debrief.js BEFORE this file.
+    
+    // The specific screen components are defined in their respective files
+    // and attached to the window object.
+    
+    console.log("Screens module loaded. Components should be available on window.");
+})();
